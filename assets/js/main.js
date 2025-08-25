84bc61b47aa5054a55cc3b8c8a20c32949d89a42
codylockyear <lilhoosier51@gmail.com>
2025-08-25 15:00:06 -0700

    fixed errors given by ghost

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// Adds momentum scroll on iOS & hides scrollbar
+// Smooth scrolling sliders
 document.querySelectorAll('.slider').forEach(slider => {
   slider.style.scrollBehavior = 'smooth';
   slider.style.webkitOverflowScrolling = 'touch';
